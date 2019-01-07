14b1ef4d00f62c1020fb5469c5216089d2ef351b
Fabian Martinez <fabianemartinez@gmail.com>
2019-01-07 11:38:26 -0500

    se crea un http inicial

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes, } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+
+export const ROUTES: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'search', component: SearchComponent },
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' },
+
+    //{ path: 'path/:routeParam', component: MyComponent },
+    //{ path: 'staticPath', component: ... },
+    //{ path: '**', component: ... },
+    //{ path: 'oldPath', redirectTo: '/staticPath' },
+    //{ path: ..., component: ..., data: { message: 'Custom' }
+];
+
