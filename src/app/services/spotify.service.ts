be85da13a96fc1dc88e82a532c9b5fce959c9947
Fabian Martinez <fabianemartinez@gmail.com>
2019-08-19 17:57:41 -0500

    update angular version

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -15,7 +15,7 @@ export class SpotifyService {
     const url = `https://api.spotify.com/v1/${query}`
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer QAuv8eJeWTtipchnxConcQr79AHAFQtFV97O5rfoTSv1MJ9iqgGMVdzxegtzjwamm2BteDW5S8Jtp-Fd-s'
+      'Authorization': 'Bearer '
     });
 
     return this.http.get(url,{headers});
